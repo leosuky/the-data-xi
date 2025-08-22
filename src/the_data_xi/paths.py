f6107833c0d589844e9c6030c9f8fd67cfa937e9
Sunkanmi Otitolaye <leosuky99@gmail.com>
2025-08-22 11:10:22 +0100

    removed spark -- too complex to work with for now

--- src/the_data_xi/paths.py
+++
@@ -1,6 +0,0 @@
-import os
-from src.config import RAW_DIR, BRONZE_DIR, SILVER_DIR, GOLD_DIR
-
-def ensure_dirs():
-    for p in (BRONZE_DIR, SILVER_DIR, GOLD_DIR):
-        os.makedirs(p, exist_ok=True)
